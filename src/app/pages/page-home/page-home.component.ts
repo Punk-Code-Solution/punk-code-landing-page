dfe4ddfbb950c335736af152f5ed920c1d54a047
Weslei Miranda <wsmiranda11@gmail.com>
2025-05-10 15:48:08 -0300

    Punk-1: Implementando Footer

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -3,6 +3,7 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { MainBannerComponent } from '../../components/main-banner/main-banner.component';
 import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 import { FaqComponent } from '../../components/faq/faq.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'page-home',
@@ -12,6 +13,7 @@ import { FaqComponent } from '../../components/faq/faq.component';
     MainBannerComponent,
     TestimonialsComponent,
     FaqComponent,
+    FooterComponent,
   ],
   templateUrl: './page-home.component.html',
   styleUrl: './page-home.component.css',
