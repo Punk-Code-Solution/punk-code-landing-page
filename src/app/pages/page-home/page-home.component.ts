ad3ec4a8a257e0e5babfadc4c6a387c5fabc013d
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-05-31 23:55:46 -0300

    Correçoes em componentes e remoções de importes

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { MainBannerComponent } from '../../components/main-banner/main-banner.component';
-import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';import { SoluctionComponentBody } from '../../components/soluction-body/soluction.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 import { SolutionComponent } from '../../components/solution/solution.component';
 import { ServiceBodyComponent } from '../../components/service-body/service-body.component';
 
