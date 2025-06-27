848c9e6be68359ec1c5229614da1b98ab385acdc
Thiago Goncalves <thiago.goncalves@cepedi.org.br>
2025-06-27 09:41:36 -0300

    PUNK-15 Iniciação da pagina

---
+++ src/app/components/service-solution/service-solution.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'service-solution',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-solution.component.html',
+  styleUrl: './service-solution.component.css'
+})
+export class ServiceSolutionComponent {
+
+}
--- src/app/pages/page-services/page-services.component.ts
+++ src/app/pages/page-services/page-services.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ServiceSolutionComponent } from '../../components/service-solution/service-solution.component';
 
 @Component({
   selector: 'page-services',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    ServiceSolutionComponent
   ],
   templateUrl: './page-services.component.html',
   styleUrl: './page-services.component.css'
