518841da89303c6f7387d4132c780486650e622f
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-06-27 10:40:21 -0300

    #PUNK-15 Criação da base da tela

--- src/app/pages/page-services/page-services.component.ts
+++ src/app/pages/page-services/page-services.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ServiceSolutionComponent } from '../../components/service-solution/service-solution.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'page-services',
   standalone: true,
   imports: [
     NavbarComponent,
-    ServiceSolutionComponent
+    ServiceSolutionComponent,
+    FooterComponent
   ],
   templateUrl: './page-services.component.html',
   styleUrl: './page-services.component.css'
