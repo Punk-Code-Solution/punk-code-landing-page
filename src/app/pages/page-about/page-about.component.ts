0a04971c61ae929926f6224c26181500ce39b8c5
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-06-28 14:57:29 -0300

    PUNK-17 informações iniciais

--- src/app/pages/page-about/page-about.component.ts
+++ src/app/pages/page-about/page-about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'page-about',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './page-about.component.html',
   styleUrl: './page-about.component.css'
