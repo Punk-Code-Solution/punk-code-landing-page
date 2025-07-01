095ae4ec1d27f1be5c1161e7b24e245c60ccebb7
Thiago Goncalves <thiago.goncalves@cepedi.org.br>
2025-06-30 21:48:40 -0300

    #PUNK-16 - criação da base da tela

--- src/app/pages/page-contact/page-contact.component.ts
+++ src/app/pages/page-contact/page-contact.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'page-contact',
   standalone: true,
   imports: [
-    NavbarComponent
-  ],
+    NavbarComponent,
+    FooterComponent
+],
   templateUrl: './page-contact.component.html',
   styleUrl: './page-contact.component.css'
 })
