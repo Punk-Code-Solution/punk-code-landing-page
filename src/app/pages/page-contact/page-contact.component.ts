ad348674129cb96983730d0f54aedd47e1892e21
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-07-04 20:46:08 -0300

    #PUNK-16 Adição de maps

--- src/app/pages/page-contact/page-contact.component.ts
+++ src/app/pages/page-contact/page-contact.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'page-contact',
   standalone: true,
   imports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    GoogleMapsModule
 ],
   templateUrl: './page-contact.component.html',
   styleUrl: './page-contact.component.css'
