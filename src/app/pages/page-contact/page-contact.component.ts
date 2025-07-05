11810f4ede2a2da2139b2281b7eeca25af994ff9
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-07-04 23:32:43 -0300

    #PUNK-16 finalização da tela de contact

--- src/app/pages/page-contact/page-contact.component.ts
+++ src/app/pages/page-contact/page-contact.component.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { GoogleMapsModule } from '@angular/google-maps';
-import { NgFor } from '@angular/common'
-import { Interface } from 'readline';
+import { NgFor } from '@angular/common';
 
-interface Imarkers {
-
-  position: google.maps.LatLng;
-
+interface Imarker { // Renomeado para seguir a convenção
+  position: google.maps.LatLngLiteral; // Use LatLngLiteral para facilitar
 }
 
 @Component({
@@ -19,18 +16,27 @@ interface Imarkers {
     FooterComponent,
     GoogleMapsModule,
     NgFor
-],
+  ],
   templateUrl: './page-contact.component.html',
   styleUrl: './page-contact.component.css'
 })
 export class PageContactComponent {
 
-  myLatLng = new google.maps.LatLng(-14.837320895671775, -39.026881435959844);
+  // Defina as coordenadas como um objeto literal
+  myLatLng: google.maps.LatLngLiteral = { lat: -14.837320895671775, lng: -39.026881435959844 };
 
-  marker: Imarkers = {
+  // Opções do mapa, incluindo o Map ID
+  mapOptions: google.maps.MapOptions = {
+    // 👇 **IMPORTANTE:** Substitua 'YOUR_MAP_ID' pelo seu ID do Mapa do Google Cloud
+    mapId: 'YOUR_MAP_ID',
+    // Você pode desabilitar a UI padrão se quiser um mapa mais limpo
+    disableDefaultUI: true
+  };
+
+  // Defina o marcador usando o objeto literal
+  marker: Imarker = {
     position: this.myLatLng
-  }
+  };
 
-  markers = [ this.marker ]
-
+  markers = [ this.marker ];
 }
