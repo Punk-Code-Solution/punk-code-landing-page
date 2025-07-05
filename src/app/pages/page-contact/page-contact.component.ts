dc2c9a30bc1952ea190dd983681dba404554fc6b
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-07-04 22:07:25 -0300

    #PUNK-16 modificação em css

--- src/app/pages/page-contact/page-contact.component.ts
+++ src/app/pages/page-contact/page-contact.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { GoogleMapsModule } from '@angular/google-maps';
+import { NgFor } from '@angular/common'
+import { Interface } from 'readline';
+
+interface Imarkers {
+
+  position: google.maps.LatLng;
+
+}
 
 @Component({
   selector: 'page-contact',
@@ -9,11 +17,20 @@ import { GoogleMapsModule } from '@angular/google-maps';
   imports: [
     NavbarComponent,
     FooterComponent,
-    GoogleMapsModule
+    GoogleMapsModule,
+    NgFor
 ],
   templateUrl: './page-contact.component.html',
   styleUrl: './page-contact.component.css'
 })
 export class PageContactComponent {
+
+  myLatLng = new google.maps.LatLng(-14.837320895671775, -39.026881435959844);
+
+  marker: Imarkers = {
+    position: this.myLatLng
+  }
+
+  markers = [ this.marker ]
 
 }
