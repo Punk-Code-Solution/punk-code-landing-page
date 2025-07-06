0d097cd9286c0ceb3d1e571915fc5512f450c381
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2025-07-05 21:50:34 -0300

    #PUNK-25 Configurações iniciais

--- src/app/pages/page-solution/page-solution.component.ts
+++ src/app/pages/page-solution/page-solution.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { SoluctionComponent } from '../../components/soluction/soluction.component';
 
 @Component({
   selector: 'page-solution',
   standalone: true,
   imports: [
-    SoluctionComponent
   ],
   templateUrl: './page-solution.component.html',
   styleUrl: './page-solution.component.css'
